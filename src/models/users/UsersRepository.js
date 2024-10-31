40b419374507e797e78e0fcb3b809fde4fb5b224
João Vitor <joaovitorportosales46@gmail.com>
2024-10-31 15:54:19 -0300

    Feat: create post user method

--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -1,11 +1,21 @@
+import User from "./User.js";
+
 class UsersRepository {
-    constructor () {
-        this.Users = [];
+    constructor() {
+        this.users = [];
     }
 
     getAllUsers() {
-        return this.Users;
-      }
+        return this.users;
+    }
+
+    addUser(name, email, password) {
+        const newUser = new User(name, email, password);
+
+        this.users.push(newUser);
+
+        return newUser;
+    }
 }
 
 export default UsersRepository;
