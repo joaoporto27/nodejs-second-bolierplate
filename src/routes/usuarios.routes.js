4eeaebb20bbc5f7e5fb03fbdb5b2f6beee122b4d
João Vitor <joaovitorportosales46@gmail.com>
2024-10-31 16:45:46 -0300

    Feat: create user by id method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -27,5 +27,25 @@ usuariosRoutes.post("/", (req, res) => {
     });
   });
 
+  usuariosRoutes.get("/:id", (req, res) => {
+    const { id } = req.params;
 
+    const user = usersList.getUserById(id);
+  
+    if (!user) {
+      return res.status(404).json({
+        message: `Usuário com id ${id} não encontrado!`,
+      });
+    }
+  
+    return res.status(200).json({
+      message: `Usuário com id ${id} encontrado!`,
+      user,
+    });
+  });
+  
+  
+  usuariosRoutes.delete("/:id", (req, res) => {
+  
+  });
 export default usuariosRoutes;
